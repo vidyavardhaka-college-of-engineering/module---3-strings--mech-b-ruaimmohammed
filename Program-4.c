2a30edc4c79dcad3f40e3a052da6fa6e06641b60
ruaimmohammed <20me0115@vvce.ac.in>
2021-03-31 11:56:53 +0000

    Program 4

--- Program-4.c
+++ Program-4.c
@@ -13,10 +13,13 @@ int main()
 {
  
     // Declare the required variables
+    char str[1000];
+    int i;
     printf("Enter a string: ");
     scanf("%s",str);
     for(i=0; str[i]!='\0'; ++i);
-       printf("The length of string = );
+       printf("The length of string = ");
+       printf("%d",i);
        //Complete the missing code in result printing statement
     return 0;
 }
